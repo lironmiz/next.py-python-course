7e74143b6a9236dc0c49f03926d21b491878ac79
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-06 05:12:06 +0200

    Create unit1_ex1.1.1.py

---
+++ unit1_ex1.1.1.py
@@ -0,0 +1,12 @@
+# exercise 1.1.1 from unit 1
+'''
+Look at the following code snippet:
+
+def secret(a):
+    return a % 3 != 0 and a % 5 != 0
+result = filter(secret, range(1, 31))
+print(list(result))
+Which of the following numbers will not appear in the list that will be output after running the program?
+'''
+
+# Answer: 12 
