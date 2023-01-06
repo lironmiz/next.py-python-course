3a81afbce9583225a9fe779e85082fe060f3844a
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-06 17:08:25 +0200

    Create unit1_ex1.2.1.py

---
+++ unit1_ex1.2.1.py
@@ -0,0 +1,21 @@
+# exercise 1.2.1 from unit 1
+'''
+Here is a section of a program written by a programmer and the output obtained after running it.
+
+a = lambda x: _______
+print(a(3))
+print(a("s"))
+print(a(2))
+print(type(a(3)))
+1
+1
+1
+<class 'int'>
+Guidelines:
+
+Do not use operators.
+Do not use helper functions (so a solution like len(str(x)) is not correct).
+Complete the missing part of the program (highlighted with a marker) so that the desired output is obtained.
+'''
+
+# Answer : 1
