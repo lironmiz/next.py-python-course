f475dad12ca7e611eb648249f133d70718d721ca
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-06 17:16:43 +0200

    Create unit1_ex1.2.2.py

---
+++ unit1_ex1.2.2.py
@@ -0,0 +1,15 @@
+# exercise 1.2.2 from unit 1
+'''
+Here is a section of a program written by a programmer and the output obtained after running it.
+
+b = lambda x: _______
+print(b(1))
+print(b(3))
+1
+3
+Guidelines: Do not use operators or helper functions.
+
+Complete the missing part of the program (highlighted with a marker) so that the desired output is obtained.
+'''
+
+# Answer x
