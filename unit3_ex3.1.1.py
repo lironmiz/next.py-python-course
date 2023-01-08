c85e061d7553a1f8cde467bc49a4f14f1f0c4aa2
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-08 10:31:57 +0200

    Create unit3_ex3.1.1.py

---
+++ unit3_ex3.1.1.py
@@ -0,0 +1,21 @@
+# exercise 3.1.1 from unit 3
+'''
+Match each of the code segments with the output obtained after running it. Answer without running!
+'''
+
+# 1:
+lst = [1, 2, 3]
+print(lst[3])
+# Answer IndexError
+
+# 2:
+print(t[5])
+# Answer NameError
+
+# 3:
+print(4 + '3')
+# Answer TypeError
+
+# 4:
+print(int('65.43'))
+# Answer ValueError
