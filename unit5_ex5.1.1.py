558ab22a69578fd0a091d7804381119795ca07bf
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-08 20:54:07 +0200

    Create unit5_ex5.1.1.py

---
+++ unit5_ex5.1.1.py
@@ -0,0 +1,13 @@
+# exercise 5.1.1 from unit 5
+'''
+Here is a line of code from a program.
+'''
+
+for item in item_lst:
+
+# Which of the following statements about the item variable is true? Mark all the correct answers.
+    
+# 1: The variable item points in each iteration to the value of the next element in the list
+
+# 2: The for loop calls the next function behind the scenes and its return value is placed in the item variable
+
