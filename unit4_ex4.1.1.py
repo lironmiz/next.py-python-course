f05da2f2598c3f5bcce75bb084cf50484e0a4893
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-08 13:37:39 +0200

    Create unit4_ex4.1.1.py

---
+++ unit4_ex4.1.1.py
@@ -0,0 +1,10 @@
+# exercise 4.1.1 from unit 4
+'''
+Here is an expression generator.
+'''
+
+gen = (i / 2 for i in [0, 9, 21, 32])
+
+# What will be returned in the second call to the next function for the gen generator?
+
+# Answer: 4.5
