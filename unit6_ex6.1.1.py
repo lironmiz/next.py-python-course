8d522d1f2abb4b89ef561701e1cc8865803518cd
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 06:26:47 +0200

    Create unit6_ex6.1.1.py

---
+++ unit6_ex6.1.1.py
@@ -0,0 +1,6 @@
+# exercise 6.1.1 from unit 6
+'''
+Which of the following is not an advantage of using built-in modules in the Python language?
+'''
+
+# Answer: Reduce the size of the program in memory
