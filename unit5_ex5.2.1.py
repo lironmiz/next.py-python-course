4e8cd5d113bf346d702f02ce3d3ba11dad890037
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 03:31:49 +0200

    Create unit5_ex5.2.1.py

---
+++ unit5_ex5.2.1.py
@@ -0,0 +1,9 @@
+# exercise 5.2.1 from unit 5
+'''
+Which of the following objects is always an iterator in Python? Mark all the correct answers.
+'''
+    
+# 1: object that return from the function iter
+
+# 2: object that return from the function filter
+
