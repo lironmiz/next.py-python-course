46fac6aed3c133863cd9e6b518a2083657050809
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 06:29:49 +0200

    Create unit6_ex6.1.2.py

---
+++ unit6_ex6.1.2.py
@@ -0,0 +1,7 @@
+# exercise 6.1.1 from unit 6
+'''
+You know that there is a module called mod and in it a function called fun. What is the
+correct way to import the function from the module?
+'''
+
+# Answer: from mod import fun
