d886705bfa6ced134c8d60a8737a0c02d7f26c7a
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 07:59:11 +0200

    Create unit6_ex6.3.1.py

---
+++ unit6_ex6.3.1.py
@@ -0,0 +1,8 @@
+# exercise 6.3.1 from unit 6
+'''
+Erez wants to write a piece of code that takes a picture and knows how to automatically identify certain objects that are in it (for example, a vehicle or a person).
+
+Which package will give him the best answer to accomplish the task?
+'''
+
+# Answer: ImageAi
