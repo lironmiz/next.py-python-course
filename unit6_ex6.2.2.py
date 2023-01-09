6f954f13a48b86e8ef849a11ac1e080fd3181912
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 07:31:11 +0200

    Create unit6_ex6.2.2.py

---
+++ unit6_ex6.2.2.py
@@ -0,0 +1,14 @@
+# exercise 6.2.2 from unit 6
+'''
+Here is the following code snippet:
+
+smile.py
+def main():
+    <your code here>
+if __name__ == "__main__":
+    main()
+What is the following conditional structure used for?
+
+'''
+
+# Answer: The condition structure is responsible for the fact that the main function in the smile.py file will not run when the file is imported as a module.
