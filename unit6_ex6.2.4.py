4d7daacb772e93c7194e880b120ab595b391b1ff
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 07:42:23 +0200

    Create unit6_ex6.2.4.py

---
+++ unit6_ex6.2.4.py
@@ -0,0 +1,16 @@
+# exercise 6.2.4 from unit 6
+'''
+Here is a Python file called search_modules and the output obtained after running it directly.
+
+search_modules.py
+import sys
+print(sys.path)
+['C:\\Heights\\Documents\\Projects\\dandush',
+'C:\\Heights\\PortableApps\\PortablePython3.7.6.1\\App\\lib\\site-packages\\pyreadline-2.0-py3.7-win32.egg',
+'C:\\Heights\\PortableApps\\PortablePython3.7.6.1\\App\\lib\\site-packages\\gnuplot_py-1.8-py3.7.egg',
+'C:\\Heights\\PortableApps\\PortablePython3.7.6.1\\App\\lib\\site-packages\\pyx-0.12.1-py3.7.egg']
+
+What can be concluded from the code regarding the location of the search_modules.py file?
+'''
+
+# Answer: The file is located in the following path: C:\Heights\Documents\Projects\dandush
