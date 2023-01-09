124ef853a90ec47a57ce0e0518679511baa5559b
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 04:19:49 +0200

    Create unit5_ex5.3.1.py

---
+++ unit5_ex5.3.1.py
@@ -0,0 +1,9 @@
+# exercise 5.3.1 from unit 5
+'''
+Look at the following sentences and mark only the correct ones among them
+'''
+  
+# 1: Every generator is an iterator
+
+# 2: Every iterator is an iterable 
+
