01eb68ecdcf0080013d1ac73d39f537b1bb2d3c1
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-09 07:35:35 +0200

    Create unit6_ex6.2.3.py

---
+++ unit6_ex6.2.3.py
@@ -0,0 +1,12 @@
+# exercise 6.2.3 from unit 6
+'''
+Here are two Python files: apple.py and orange.py.
+
+apple.py
+print(__name__)
+orange.py
+import apple
+
+When will the apple output be printed?
+'''
+# Answer: When we run the orange.py file
